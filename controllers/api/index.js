ef5f5ac90ff1184d53e2b2a5907fa5e16f43f8b4
JosephW143 <josephwright1436@gmail.com>
2022-09-15 19:09:09 -0400

    add comment route file and updated index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const postRoutes = require('./vote-routes');
+const postRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', voteRoutes);
+router.use('/posts', commentRoutes);
 
 module.exports = router;
