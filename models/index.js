4b90dc8a50dd5ef47220adaf2d0849eb75ef7a67
Theo <tdgreer1203@gmail.com>
2022-09-17 11:28:05 -0400

    Working on Returning Voted Users

--- models/index.js
+++ models/index.js
@@ -2,20 +2,20 @@ const User = require("./User");
 const Vote = require('./Vote');
 const Comment = require('./Comment');
 
-Vote.belongsTo(User, {
-    foreignKey: 'user_id'
+User.hasMany(Comment, {
+    foreignKey: 'recipient_id'
 });
 
 User.hasMany(Vote, {
     foreignKey: 'user_id'
 });
 
-Comment.belongsTo(User, {
-    foreignKey: 'recipient_id'
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
 });
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id'
+Comment.belongsTo(User, {
+    foreignKey: 'recipient_id'
 });
 
 module.exports = { User, Vote, Comment };
