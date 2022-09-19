ed9fff1b8a0eaaa1cdf1479955462cf482fc6ee5
JosephW143 <josephwright1436@gmail.com>
2022-09-19 18:57:14 -0400

    added handlebars to test front end

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -9,6 +9,10 @@ router.get('/login', (req, res) => {
     }
   
     res.render('login');
-  });
+});
+
+router.get('/', (req, res) => {
+  res.render('homepage');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//logic to use handlebars
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
