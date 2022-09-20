02ffba93e01d893c296b31e40302c9c559ac92d4
JosephW143 <josephwright1436@gmail.com>
2022-09-20 19:07:21 -0400

    updated home routes and homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,17 +2,30 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Comment, Vote } = require('../models');
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: [
+      'id',
+      'name',
+      'breed',
+      'age',
+      'profile',
+      'image_url'
+    ]
+  })
+  .then(dbUserData => {
+    const users = dbUserData.map(user => user.get({ plain: true }));
+
+    res.render('homepage', { users });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
-router.get('/', (req, res) => {
-  res.render('homepage');
+router.get('/login', (req, res) => {
+  res.render('login');
 });
 
 module.exports = router;
