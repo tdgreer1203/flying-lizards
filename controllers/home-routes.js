3f4f7f18c84fe9ee8c3e46f8b459882a1727bc91
Theo <tdgreer1203@gmail.com>
2022-09-20 23:15:32 -0400

    Added Bootstrap

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Comment, Vote } = require('../models');
+const userRoutes = require('./api/user-routes');
 
 router.get('/', (req, res) => {
   User.findAll({
@@ -15,7 +16,7 @@ router.get('/', (req, res) => {
   })
   .then(dbUserData => {
     const users = dbUserData.map(user => user.get({ plain: true }));
-
+    
     res.render('homepage', { users });
   })
   .catch(err => {
@@ -26,6 +27,30 @@ router.get('/', (req, res) => {
 
 router.get('/login', (req, res) => {
   res.render('login');
+});
+
+router.get('/profile/:id', async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id, {
+        include: [
+            {
+                model: Comment, 
+                attributes: ['comment_text', 'author_id']
+            },
+            {
+                model: User,
+                attributes: ['name', 'image_url'],
+                through: Vote,
+                as: 'recipient'
+            }
+        ]
+    });
+    const profile = user.get({ plain: true });
+    res.render('profile', { profile });
+} catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+}
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,26 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//logic to use handlebars
-const hbs = exphbs.create({});
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
