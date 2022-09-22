5d34887fa514a3b749fe548564bcf87fbbc03d40
JosephW143 <josephwright1436@gmail.com>
2022-09-22 18:46:28 -0400

    new comment.js

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -2,16 +2,20 @@ async function commentFormHandler(event) {
     event.preventDefault();
   
     const comment_text = document.querySelector('textarea[name="commentText"]').value.trim();
-    const post_id = window.location.toString().split('/')[
+    const recipient_id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
-  
+    const author_id = Math.floor(Math.random() * 10) + 1;
+    
+    console.log(recipient_id);
+
     if (comment_text) {
       const response = await fetch('/api/comments', {
         method: 'POST',
         body: JSON.stringify({
-          post_id,
-          comment_text
+          recipient_id,
+          comment_text,
+          author_id
         }),
         headers: {
           'Content-Type': 'application/json'
