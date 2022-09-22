a2475a9f8f0d41cd35f5a8ca8526b4b98cca2acb
Theo <tdgreer1203@gmail.com>
2022-09-22 07:13:11 -0400

    Finished Comments section

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Comment, Vote } = require('../models');
+const { QueryTypes } = require('sequelize');
 
 router.get('/', (req, res) => {
   User.findAll({
@@ -39,23 +40,16 @@ router.get('/login', (req, res) => {
 router.get('/profile/:id', async (req, res) => {
   try {
     const user = await User.findByPk(req.params.id, {
-        include: [
-            {
-                model: Comment, 
-                attributes: ['comment_text', 'author_id', 'createdAt'],
-                order: [['createdAt', 'DESC']],
-                include: [
-                  { 
-                    model: User,
-                    as: 'user'
-                  }
-                ]
-            }
-        ]
+      attributes: { exclude: ['password'] }
     });
+    const comments = await sequelize.query(`SELECT comment.author_id, comment_text, username, image_url, created_at FROM comment LEFT JOIN user ON comment.author_id = user.id WHERE recipient_id = ? ORDER BY created_at DESC`, {
+      replacements: [req.params.id],
+      type: QueryTypes.SELECT
+    }); 
     const profile = user.get({ plain: true });
     console.log(profile);
-    res.render('profile', { profile });
+    console.log(comments);
+    res.render('profile', { profile, comments });
 } catch (err) {
     console.log(err);
     res.status(500).json(err);
