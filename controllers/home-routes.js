8d1b016455eb1405f0ae5388d4b975b5d082b0c4
Theo <tdgreer1203@gmail.com>
2022-09-21 20:40:13 -0400

    Trying to fix CSS

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -28,4 +28,37 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/profile/:id', async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id, {
+        include: [
+            {
+                model: Comment, 
+                attributes: ['comment_text', 'author_id']
+            },
+            {
+                model: User,
+                attributes: ['name', 'image_url'],
+                through: Vote,
+                as: 'recipient'
+            }
+        ]
+    });
+    const profile = user.get({ plain: true });
+    res.render('profile', { profile });
+} catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+}
+});
+
 module.exports = router;
